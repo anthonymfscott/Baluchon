7914359402a1ed34821874972bc0a7d53ecabfb3
anthonymfscott <anthonymfscott@gmail.com>
2020-06-08 01:49:18 +0200

    UI: Add TranslationViewController

---
+++ Baluchon/Controller/TranslationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TranslationViewController.swift
+//  Baluchon
+//
+//  Created by anthonymfscott on 08/06/2020.
+//  Copyright © 2020 Anthony Scott. All rights reserved.
+//
+
+import UIKit
+
+class TranslationViewController: UIViewController {
+    @IBOutlet var targetLanguageStackView: UIStackView!
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
