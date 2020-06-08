51368a5ac38d16b90017e7cd8f3c56ef93c7fb39
anthonymfscott <anthonymfscott@gmail.com>
2020-06-08 13:26:13 +0200

    Update UI: first completion of WeatherViewController

---
+++ Baluchon/Controller/WeatherViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by anthonymfscott on 05/06/2020.
+//  Copyright © 2020 Anthony Scott. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    override func viewDidLoad() {
+
+    }
+}
