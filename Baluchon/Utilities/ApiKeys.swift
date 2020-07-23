ea4d0d736bc5c3c0aedd9db733a3143ea9b9922f
anthonymfscott <anthonymfscott@gmail.com>
2020-07-23 12:42:16 +0200

    Delete ASApiKeys.swift

--- Baluchon/Utilities/ApiKeys.swift
+++ Baluchon/Utilities/ApiKeys.swift
@@ -9,7 +9,6 @@
 import Foundation
 
 func valueForAPIKey(named keyname: String) -> String? {
-    guard let filePath = Bundle.main.path(forResource: "ASApiKeys", ofType: "plist"), let plist = NSDictionary(contentsOfFile: filePath), let value = plist.object(forKey: keyname) as? String else { return nil }
+    guard let filePath = Bundle.main.path(forResource: "ApiKeys", ofType: "plist"), let plist = NSDictionary(contentsOfFile: filePath), let value = plist.object(forKey: keyname) as? String else { return nil }
     return value
 }
-
